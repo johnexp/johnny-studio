f15d945f6c8b240306eafcf42190c3809306793a
Jonathan Junio <jonathan.ladeira@basis.com.br>
2017-03-06 17:42:50 -0300

    Model e View field type.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app works!';
-  fieldTypes: any[];
-  fieldType: string = '';
+  title = '';
+  modelFieldTypes: any[];
+  viewFieldTypes: any[];
+  modelFieldType: string = '';
+  viewFieldType: string = '';
+  hasViewField: boolean = true;
+  modelViewFieldRel: {};
 
   ngOnInit() {
-    this.initFieldTypes();
+    this.initModelFieldTypes();
+    this.initViewFieldTypes();
+    this.initModelViewFieldRel();
   }
 
-  initFieldTypes() {
-    this.fieldTypes = [
+  initModelFieldTypes() {
+    this.modelFieldTypes = [
+      { value: 'string', viewValue: 'String' },
+      { value: 'enum', viewValue: 'Enum' },
+      { value: 'number', viewValue: 'Number' },
+      { value: 'date', viewValue: 'Date' },
+      { value: 'buffer', viewValue: 'Buffer' },
+      { value: 'boolean', viewValue: 'Boolean' },
+      { value: 'mixed', viewValue: 'Mixed' },
+      { value: 'objectid', viewValue: 'ObjectId' },
+      { value: 'array', viewValue: 'Array' }
+    ];
+  }
+
+  initViewFieldTypes() {
+    this.viewFieldTypes = [
       { value: 'text', viewValue: 'Text' },
       { value: 'number', viewValue: 'Number' },
       { value: 'select', viewValue: 'Select Item' },
@@ -23,8 +43,27 @@ export class AppComponent implements OnInit {
       { value: 'checkbox', viewValue: 'Checkbox' },
       { value: 'textarea', viewValue: 'TextArea' },
       { value: 'slider', viewValue: 'Slider' },
-      { value: 'toggle', viewValue: 'Toggle' }
+      { value: 'toggle', viewValue: 'Toggle' },
+      { value: 'chips', viewValue: 'Chips' },
+      { value: 'datepicker', viewValue: 'Datepicker' },
+      { value: 'gridmodal', viewValue: 'GridModal' }
     ];
+  }
+
+  initModelViewFieldRel() {
+    this.modelViewFieldRel = {
+      string: ['text', 'textarea'],
+      enum: ['select', 'radiobutton'],
+      number: ['number', 'slider'],
+      date: ['datepicker'],
+      boolean: ['checkbox', 'toggle'],
+      objectid: ['select', 'gridmodal'],
+      array: {
+        string: ['chips'],
+        enum: ['checkbox', 'select'],
+        objectid: ['select', 'gridmodal']
+      }
+    }
   }
 
 }
